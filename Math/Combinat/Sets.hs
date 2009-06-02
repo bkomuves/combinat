64c32f9180bea0ea7d85df925a4593052ab37d74
Balazs Komuves <bkomuves@gmail.com>
2009-06-02 19:37:55 +0000

    added `combine' and `tuplesFromList' to Sets.hs

--- Math/Combinat/Sets.hs
+++ Math/Combinat/Sets.hs
@@ -2,7 +2,11 @@
 -- | Subsets. 
 
 module Math.Combinat.Sets 
-  ( choose
+  ( 
+    choose
+  , combine
+  , tuplesFromList
+  
   , kSublists
   , sublists
   , countKSublists
@@ -12,17 +16,35 @@ module Math.Combinat.Sets
 
 import Math.Combinat.Helper
 
--------------------------------------------------------
+--------------------------------------------------------------------------------
 
--- | synonym for "kSublists"
+-- | All possible ways to choose @k@ elements from a list, without
+-- repetitions. \"Antisymmetric power\" for lists. Synonym for "kSublists".
 choose :: Int -> [a] -> [[a]]
-choose = kSublists
+choose 0 _  = [[]]
+choose k [] = []
+choose k (x:xs) = map (x:) (choose (k-1) xs) ++ choose k xs  
+
+-- | All possible ways to choose @k@ elements from a list, /with
+-- repetitions/. \"Symmetric power\" for lists. See also "Math.Combinat.Combinations".
+-- TODO: better name?
+combine :: Int -> [a] -> [[a]]
+combine 0 _  = [[]]
+combine k [] = []
+combine k xxs@(x:xs) = map (x:) (combine (k-1) xxs) ++ combine k xs  
+
+-- | \"Tensor power\" for lists.
+-- See also "Math.Combinat.Tuples".
+-- TODO: better name?
+tuplesFromList :: Int -> [a] -> [[a]]
+tuplesFromList 0 _  = [[]]
+tuplesFromList k xs = [ (y:ys) | y <- xs, ys <- tuplesFromList (k-1) xs ]
+ 
+--------------------------------------------------------------------------------
 
 -- | Sublists of a list having given number of elements.
 kSublists :: Int -> [a] -> [[a]]
-kSublists 0 _  = [[]]
-kSublists k [] = []
-kSublists k (x:xs) = map (x:) (kSublists (k-1) xs) ++ kSublists k xs  
+kSublists = choose
 
 -- | @# = \binom { n } { k }@.
 countKSublists :: Int -> Int -> Integer
@@ -37,4 +59,4 @@ sublists (x:xs) = map (x:) (sublists xs) ++ sublists xs
 countSublists :: Int -> Integer
 countSublists n = 2 ^ n
 
--------------------------------------------------------
+--------------------------------------------------------------------------------
