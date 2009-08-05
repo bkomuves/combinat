cdfeb7fcc71bfde63274041e5196e5daa8ea7bb4
Balazs Komuves <bkomuves@gmail.com>
2009-08-05 17:16:52 +0000

    graphviz visualization for binary trees

--- Math/Combinat/Graphviz.hs
+++ Math/Combinat/Graphviz.hs
@@ -3,6 +3,8 @@
 
 module Math.Combinat.Graphviz 
   ( Dot
+  , binTreeDot
+  , binTree'Dot
   , treeDot
   , forestDot
   )
@@ -16,7 +18,7 @@ import Control.Applicative
 import Control.Monad.State
 import Data.Traversable (traverse)
 
-import Math.Combinat.Trees.Binary (BinTree, BinTree')
+import Math.Combinat.Trees.Binary (BinTree(..), BinTree'(..))
 import Math.Combinat.Trees.Nary (addUniqueLabelsTree, addUniqueLabelsForest)
 
 --------------------------------------------------------------------------------
@@ -28,6 +30,48 @@ digraphBracket name lines =
   "digraph " ++ name ++ " {\n" ++ 
   concatMap (\xs -> "  "++xs++"\n") lines    
   ++ "}\n"
+  
+--------------------------------------------------------------------------------
+
+binTreeDot :: Show a => String -> BinTree a -> Dot
+binTreeDot graphname tree = 
+  digraphBracket graphname $ binTreeDot' tree
+
+binTree'Dot :: (Show a, Show b) => String -> BinTree' a b -> Dot
+binTree'Dot graphname tree = 
+  digraphBracket graphname $ binTree'Dot' tree
+  
+binTreeDot' :: Show a => BinTree a -> [String]
+binTreeDot' tree = lines where
+  lines = worker (0::Int) "r" tree 
+  name path = "node_"++path
+  worker depth path (Leaf x) = 
+    [ name path ++ "[shape=box,label=\"" ++ show x ++ "\"" ++ "];" ]
+  worker depth path (Branch left right) 
+    = [vertex,leftedge,rightedge] ++ 
+      worker (depth+1) ('l':path) left ++ 
+      worker (depth+1) ('r':path) right
+    where 
+      vertex = name path ++ "[shape=circle,style=filled,height=0.25,label=\"\"];"
+      leftedge  = name path ++ " -> " ++ name ('l':path) ++ "[tailport=sw];"
+      rightedge = name path ++ " -> " ++ name ('r':path) ++ "[tailport=se];"
+
+binTree'Dot' :: (Show a, Show b) => BinTree' a b -> [String]
+binTree'Dot' tree = lines where
+  lines = worker (0::Int) "r" tree 
+  name path = "node_"++path
+  worker depth path (Leaf' x) = 
+    [ name path ++ "[shape=box,label=\"" ++ show x ++ "\"" ++ "];" ]
+  worker depth path (Branch' left y right) 
+    = [vertex,leftedge,rightedge] ++ 
+      worker (depth+1) ('l':path) left ++ 
+      worker (depth+1) ('r':path) right
+    where 
+      vertex = name path ++ "[shape=ellipse,label=\"" ++ show y ++ "\"];"
+      leftedge  = name path ++ " -> " ++ name ('l':path) ++ "[tailport=sw];"
+      rightedge = name path ++ " -> " ++ name ('r':path) ++ "[tailport=se];"
+
+--------------------------------------------------------------------------------
     
 -- | Generates graphviz @.dot@ file from a forest. The first argument tells whether
 -- to make the individual trees clustered subgraphs; the second is the name of the
@@ -47,7 +91,8 @@ forestDot clustered graphname forest = digraphBracket graphname lines where
 -- | Generates graphviz @.dot@ file from a tree. The first argument is
 -- the name of the graph.
 treeDot :: Show a => String -> Tree a -> Dot
-treeDot graphname tree = digraphBracket graphname $ worker (0::Int) (addUniqueLabelsTree tree) where
+treeDot graphname tree = digraphBracket graphname lines where
+  lines = worker (0::Int) (addUniqueLabelsTree tree) 
   name unique = "node_"++show unique
   worker depth (Node (label,unique) subtrees) = vertex : edges ++ concatMap (worker (depth+1)) subtrees where
     vertex = name unique ++ "[label=\"" ++ show label ++ "\"" ++ "];"
