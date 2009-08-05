ac7e3340e3d429d4b31ad6e6dd9f942cfef35707
Balazs Komuves <bkomuves@gmail.com>
2009-08-05 02:06:43 +0000

    added vector partitions and partitions of multisets

--- Math/Combinat/Partitions.hs
+++ Math/Combinat/Partitions.hs
@@ -1,5 +1,8 @@
 
 -- | Partitions. Partitions are nonincreasing sequences of positive integers.
+--
+-- See also 
+--   Donald E. Knuth: The Art of Computer Programming, vol 4, pre-fascicle 3B.
 
 module Math.Combinat.Partitions
   ( -- * Type and basic stuff
@@ -30,14 +33,21 @@ module Math.Combinat.Partitions
   , allPartitions 
   , countAllPartitions'
   , countAllPartitions
+    -- * Paritions of multisets
+  , partitionMultiset
+  , IntVector
+  , vectorPartitions
+  , fasc3B_algorithm_M
   ) 
   where
 
 import Data.List
+import Data.Array.Unboxed
+
 import Math.Combinat.Helper
 import Math.Combinat.Numbers (factorial,binomial,multinomial)
 
--------------------------------------------------------
+--------------------------------------------------------------------------------
 
 -- | The additional invariant enforced here is that partitions 
 --   are monotone decreasing sequences of positive integers.
@@ -115,7 +125,7 @@ countAutomorphisms = _countAutomorphisms . fromPartition
 _countAutomorphisms :: [Int] -> Integer
 _countAutomorphisms = multinomial . map length . group
  
--------------------------------------------------------
+---------------------------------------------------------------------------------
 
 -- | Partitions of d, fitting into a given rectangle, as lists.
 _partitions' 
@@ -172,5 +182,78 @@ countAllPartitions' (h,w) =
 countAllPartitions :: Int -> Integer
 countAllPartitions d = sum [ countPartitions i | i <- [0..d] ]
 
--------------------------------------------------------
+--------------------------------------------------------------------------------
 
+-- | Partitions of a multiset.
+partitionMultiset :: (Eq a, Ord a) => [a] -> [[[a]]]
+partitionMultiset xs = parts where
+  parts = (map . map) (f . elems) temp
+  f ns = concat (zipWith replicate ns zs)
+  temp = fasc3B_algorithm_M counts
+  counts = map length ys
+  ys = group (sort xs) 
+  zs = map head ys
+
+-- | Integer vectors. The indexing starts from 1.
+type IntVector = UArray Int Int
+
+-- | Vector partitions. Basically a synonym for 'fasc3B_algorithm_M'.
+vectorPartitions :: IntVector -> [[IntVector]]
+vectorPartitions = fasc3B_algorithm_M . elems
+
+-- | Generates all vector partitions 
+--   (\"algorithm M\" in Knuth). 
+--   The order is decreasing lexicographic.  
+fasc3B_algorithm_M :: [Int] -> [[IntVector]] 
+{- note to self: Knuth's descriptions of algorithms are still totally unreadable -}
+fasc3B_algorithm_M xs = worker [start] where
+
+  -- n = sum xs
+  m = length xs
+
+  start = [ (j,x,x) | (j,x) <- zip [1..] xs ]  
+  
+  worker stack@(last:_) = 
+    case decrease stack' of
+      Nothing -> [visited]
+      Just stack'' -> visited : worker stack''
+    where
+      stack'  = subtract_rec stack
+      visited = map to_vector stack'
+      
+  decrease (last:rest) = 
+    case span (\(_,_,v) -> v==0) (reverse last) of
+      ( _ , [(_,_,1)] ) -> case rest of
+        [] -> Nothing
+        _  -> decrease rest
+      ( second , (c,u,v):first ) -> Just (modified:rest) where 
+        modified =   
+          reverse first ++ 
+          (c,u,v-1) :  
+          [ (c,u,u) | (c,u,_) <- reverse second ] 
+      _ -> error "should not happen"
+        
+  to_vector cuvs = 
+    accumArray (flip const) 0 (1,m)
+      [ (c,v) | (c,_,v) <- cuvs ] 
+
+  subtract_rec all@(last:_) = 
+    case subtract last of 
+      []  -> all
+      new -> subtract_rec (new:all) 
+
+  subtract [] = []
+  subtract full@((c,u,v):rest) = 
+    if w >= v 
+      then (c,w,v) : subtract   rest
+      else           subtract_b full
+    where w = u - v
+    
+  subtract_b [] = []
+  subtract_b ((c,u,v):rest) = 
+    if w /= 0 
+      then (c,w,w) : subtract_b rest
+      else           subtract_b rest
+    where w = u - v
+
+--------------------------------------------------------------------------------
