b95759772342750482b3997fc41e4c0f89133ed3
Balazs Komuves <bkomuves@gmail.com>
2015-10-28 11:47:33 +0000

    make gen_figures.hs be compatible with diagrams 1.3

--- svg/src/gen_figures.hs
+++ svg/src/gen_figures.hs
@@ -40,13 +40,13 @@ boxSep m xs = pad 1.05 $ vcatSep $ map hcatSep $ yys where
 
 main = do 
 
-  export "plane_partition.svg" (Width 320) $ drawPlanePartition3D $
+  export "plane_partition.svg" (mkWidth 320) $ drawPlanePartition3D $
     PlanePart [[5,4,3,3,1],[4,4,2,1],[3,2],[2,1],[1],[1]] 
 
-  export "noncrossing.svg" (Width 256) $ pad 1.10 $ drawNonCrossingCircleDiagram' orange True $
+  export "noncrossing.svg" (mkWidth 256) $ pad 1.10 $ drawNonCrossingCircleDiagram' orange True $
     NonCrossing [[3],[5,4,2],[7,6,1],[9,8]]
 
-  export "young_tableau.svg" (Width 256) $ drawTableau $ 
+  export "young_tableau.svg" (mkWidth 256) $ drawTableau $ 
     [ [ 1 , 3 , 4 , 6 , 7 ]
     , [ 2 , 5 , 8 ,10 ]
     , [ 9 ]
@@ -55,12 +55,12 @@ main = do
   let u = UpStep
       d = DownStep
       path = [ u,u,d,u,u,u,d,u,d,d,u,d,u,u,u,d,d,d,d,d,u,d,u,u,d,d ]     
-  export "dyck_path.svg" (Width 500) $ drawLatticePath $ path
+  export "dyck_path.svg" (mkWidth 500) $ drawLatticePath $ path
   -- print (pathHeight path, pathNumberOfZeroTouches path, pathNumberOfPeaks path)
 
-  export "ferrers.svg" (Width 256) $ drawFerrersDiagram' EnglishNotation red True $
+  export "ferrers.svg" (mkWidth 256) $ drawFerrersDiagram' EnglishNotation red True $
     Partition [8,6,3,3,1]
 
-  export "bintrees.svg" (Width 750) $ boxSep 7 $ map drawBinTree_ (binaryTrees 4)
+  export "bintrees.svg" (mkWidth 750) $ boxSep 7 $ map drawBinTree_ (binaryTrees 4)
 
 --------------------------------------------------------------------------------
