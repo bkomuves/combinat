4e3aa59f06217962e6b2170037572702f800b090
Balazs Komuves <bkomuves@gmail.com>
2019-11-20 15:29:48 +0000

    add missing Numbers module, plus re-export the Primes module

---
+++ Math/Combinat/Numbers.hs
@@ -0,0 +1,12 @@
+
+module Math.Combinat.Numbers 
+  ( module Math.Combinat.Numbers.Integers
+  , module Math.Combinat.Numbers.Primes
+  , module Math.Combinat.Numbers.Sequences
+  ) 
+  where
+
+import Math.Combinat.Numbers.Integers
+import Math.Combinat.Numbers.Primes
+import Math.Combinat.Numbers.Sequences
+
